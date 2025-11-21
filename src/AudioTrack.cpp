95e4da458e66af9670d01c8321f1a347d5fd0b5f
shir_sz <shir100.dz@gmail.com>
2025-11-21 11:54:30 +0000

    shir initial commit

--- src/AudioTrack.cpp
+++ src/AudioTrack.cpp
@@ -35,16 +35,28 @@ AudioTrack::~AudioTrack() {
     #ifdef DEBUG
     std::cout << "AudioTrack destructor called for: " << title << std::endl;
     #endif
-    // Your code here...
+    delete [] waveform_data;
+    waveform_data = nullptr;
 }
 
 AudioTrack::AudioTrack(const AudioTrack& other)
+: title(other.title),
+      artists(other.artists),
+      duration_seconds(other.duration_seconds),
+      bpm(other.bpm),
+      waveform_size(other.waveform_size),
+      waveform_data(nullptr)
 {
     // TODO: Implement the copy constructor
     #ifdef DEBUG
     std::cout << "AudioTrack copy constructor called for: " << other.title << std::endl;
     #endif
-    // Your code here...
+    if(other.waveform_data && waveform_size > 0) {
+        waveform_data = new double[waveform_size];
+        for(size_t i = 0; i < waveform_size; i++) {
+            waveform_data[i] = other.waveform_data[i];
+        }
+    }
 }
 
 AudioTrack& AudioTrack::operator=(const AudioTrack& other) {
@@ -52,25 +64,57 @@ AudioTrack& AudioTrack::operator=(const AudioTrack& other) {
     #ifdef DEBUG
     std::cout << "AudioTrack copy assignment called for: " << other.title << std::endl;
     #endif
-    // Your code here...
+    if(this != &other) {
+        title = other.title;
+        artists = other.artists;
+        duration_seconds = other.duration_seconds;
+        bpm = other.bpm;
+        waveform_size = other.waveform_size;
+        delete[] waveform_data; //שחרור זיכרון שהוקצה מראש עבור מערך שלם
+        if(other.waveform_data && waveform_size > 0) {
+            waveform_data = new double[waveform_size];
+            for(size_t i = 0; i < waveform_size; i++) {
+            waveform_data[i] = other.waveform_data[i];
+            } 
+        } else {waveform_data = nullptr;
+        }
+    }
     return *this;
 }
 
-AudioTrack::AudioTrack(AudioTrack&& other) noexcept {
+AudioTrack::AudioTrack(AudioTrack&& other) noexcept 
+: title(std::move(other.title)), //קריאה למוב הפנימי של סטרינג
+      artists(std::move(other.artists)),
+      duration_seconds(other.duration_seconds),
+      bpm(other.bpm),
+      waveform_size(other.waveform_size), 
+      waveform_data(other.waveform_data){
     // TODO: Implement the move constructor
     #ifdef DEBUG
     std::cout << "AudioTrack move constructor called for: " << other.title << std::endl;
     #endif
-    // Your code here...
+    other.waveform_data = nullptr;
+    other.waveform_size = 0;
 }
 
-AudioTrack& AudioTrack::operator=(AudioTrack&& other) noexcept {
+AudioTrack& AudioTrack::operator=(AudioTrack&& other) noexcept 
+ {
     // TODO: Implement the move assignment operator
 
     #ifdef DEBUG
     std::cout << "AudioTrack move assignment called for: " << other.title << std::endl;
     #endif
-    // Your code here...
+    if (this != &other) {
+        delete[] this->waveform_data;
+        title = std::move(other.title);
+        artists = std::move(other.artists);
+        duration_seconds = other.duration_seconds;
+        bpm = other.bpm;
+        waveform_size = other.waveform_size;
+        waveform_data = other.waveform_data;
+        other.waveform_data = nullptr;
+        other.waveform_size = 0;
+    }
     return *this;
 }
 
